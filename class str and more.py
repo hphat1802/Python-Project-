cd0efc8c1bfa736d125406b3062dc89e646711dd
hphat1802 <phatht23413e@st.uel.edu.vn>
2024-04-04 20:13:48 +0700

    first commit

---
+++ class str and more.py
@@ -0,0 +1,14 @@
+class eco:
+    def __init__(self,taste,demand,supply,price):
+        self.taste=taste
+        self.demand=demand
+        self.supply=supply
+        self.price=price
+    def price_when_demand(self):
+        if self.demand == 2:
+
+
+
+
+apple=eco(5,6,7)
+banana=eco(8,9,10)
